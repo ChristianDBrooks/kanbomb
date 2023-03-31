b7560a6cf74028a43d51edfbbe64e6ec33686ee1
Christian Brooks <christiandbrooks@gmail.com>
2023-03-31 11:15:14 -0400

    convert dashboard to static page and see if it works

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,15 @@
-import Message, { useMessageProvider } from "@components/Message";
-import TaskList from "@components/TaskList";
-import AddIcon from '@mui/icons-material/Add';
-import { Box, colors, Container, Stack, Typography } from "@mui/material";
-import { Board, Task, TaskList as TaskListModel } from "@prisma/client";
-import { useState } from "react";
 
-type BoardsWithTaskListsWithTasks = (Board & {
-  taskLists: (TaskListModel & {
-    tasks: Task[];
-  })[];
-})[]
-
-export function getServerSideProps() {
-  console.log('[SSR]: loading server side dashboard')
-  return {
-    props: { boards: [] }
-  }
+export default function TestDashboard() {
+  return (<div>
+    This is the static dashboard.
+  </div>)
 }
+
+// type BoardsWithTaskListsWithTasks = (Board & {
+//   taskLists: (TaskListModel & {
+//     tasks: Task[];
+//   })[];
+// })[]
 
 // export const getServerSideProps = withSessionSsr(
 //   function getServerSideProps(ctx) {
@@ -52,106 +45,106 @@ export function getServerSideProps() {
 //   },
 // );
 
-export default function DashboardPage({ boards: inititalBoards }: { boards: BoardsWithTaskListsWithTasks }) {
-  const [boards, setBoards] = useState(inititalBoards);
-  const [activeBoardId, setActiveBoardId] = useState(boards?.[0]?.id)
-  const activeBoard = boards?.find((board) => board.id === activeBoardId)
-  const { showMessage, messageController } = useMessageProvider();
+// export default function DashboardPage({ boards: inititalBoards }: { boards: BoardsWithTaskListsWithTasks }) {
+//   const [boards, setBoards] = useState(inititalBoards);
+//   const [activeBoardId, setActiveBoardId] = useState(boards?.[0]?.id)
+//   const activeBoard = boards?.find((board) => board.id === activeBoardId)
+//   const { showMessage, messageController } = useMessageProvider();
 
-  const handleAddTaskList = async () => {
-    const addTaskListResponse = await fetch('/api/tasklist', {
-      method: 'POST',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({
-        boardId: activeBoardId
-      })
-    })
+//   const handleAddTaskList = async () => {
+//     const addTaskListResponse = await fetch('/api/tasklist', {
+//       method: 'POST',
+//       headers: { 'content-type': 'application/json' },
+//       body: JSON.stringify({
+//         boardId: activeBoardId
+//       })
+//     })
 
-    if (!addTaskListResponse.ok) {
-      showMessage('Failed to add task list!', 'error')
-      return;
-    }
+//     if (!addTaskListResponse.ok) {
+//       showMessage('Failed to add task list!', 'error')
+//       return;
+//     }
 
-    const result: {
-      data: {
-        boards: BoardsWithTaskListsWithTasks
-      }
-    } = await addTaskListResponse.json();
-    setBoards(result.data.boards)
-  }
+//     const result: {
+//       data: {
+//         boards: BoardsWithTaskListsWithTasks
+//       }
+//     } = await addTaskListResponse.json();
+//     setBoards(result.data.boards)
+//   }
 
-  const handleDeleteTaskList = async (id: string) => {
-    const deleteTaskListResponse = await fetch('/api/tasklist', {
-      method: "DELETE",
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({
-        taskListId: id
-      })
-    })
+//   const handleDeleteTaskList = async (id: string) => {
+//     const deleteTaskListResponse = await fetch('/api/tasklist', {
+//       method: "DELETE",
+//       headers: { 'content-type': 'application/json' },
+//       body: JSON.stringify({
+//         taskListId: id
+//       })
+//     })
 
 
-    if (!deleteTaskListResponse.ok) {
-      showMessage('Failed to delete task list!', 'error')
-      return;
-    }
+//     if (!deleteTaskListResponse.ok) {
+//       showMessage('Failed to delete task list!', 'error')
+//       return;
+//     }
 
-    const result: {
-      data: {
-        boards: BoardsWithTaskListsWithTasks
-      }
-    } = await deleteTaskListResponse.json();
-    setBoards(result.data.boards)
-  }
+//     const result: {
+//       data: {
+//         boards: BoardsWithTaskListsWithTasks
+//       }
+//     } = await deleteTaskListResponse.json();
+//     setBoards(result.data.boards)
+//   }
 
-  if (!activeBoard) {
-    return (
-      <Container sx={{
-        padding: 2,
-        minHeight: 'calc(100vh - 64px)'
-      }}>
-        <Typography>
-          You have no boards. Create one!
-        </Typography>
-      </Container>
-    )
-  }
+//   if (!activeBoard) {
+//     return (
+//       <Container sx={{
+//         padding: 2,
+//         minHeight: 'calc(100vh - 64px)'
+//       }}>
+//         <Typography>
+//           You have no boards. Create one!
+//         </Typography>
+//       </Container>
+//     )
+//   }
 
-  return (
-    <Container sx={{
-      minHeight: 'calc(100vh - 64px)',
-    }}>
-      <Box overflow='auto'>
-        <Message controller={messageController} />
-        <Typography
-          variant="h4"
-          paddingY={2}
-        >{activeBoard.title}</Typography>
-        <Stack direction="row" gap={2}>
-          {
-            activeBoard.taskLists.map(taskList => <TaskList
-              key={taskList.id}
-              data={taskList}
-              deleteList={handleDeleteTaskList}
-            />)
-          }
-          <Box
-            onClick={() => handleAddTaskList()}
-            sx={{
-              background: colors.grey[800],
-              padding: 2,
-              borderRadius: '4px',
-              minWidth: 300,
-              cursor: 'pointer',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          >
-            <AddIcon />
-            Add new list
-          </Box>
-        </Stack>
-      </Box>
-    </Container>
-  )
-}
+//   return (
+//     <Container sx={{
+//       minHeight: 'calc(100vh - 64px)',
+//     }}>
+//       <Box overflow='auto'>
+//         <Message controller={messageController} />
+//         <Typography
+//           variant="h4"
+//           paddingY={2}
+//         >{activeBoard.title}</Typography>
+//         <Stack direction="row" gap={2}>
+//           {
+//             activeBoard.taskLists.map(taskList => <TaskList
+//               key={taskList.id}
+//               data={taskList}
+//               deleteList={handleDeleteTaskList}
+//             />)
+//           }
+//           <Box
+//             onClick={() => handleAddTaskList()}
+//             sx={{
+//               background: colors.grey[800],
+//               padding: 2,
+//               borderRadius: '4px',
+//               minWidth: 300,
+//               cursor: 'pointer',
+//               display: 'flex',
+//               justifyContent: 'center',
+//               alignItems: 'center'
+//             }}
+//           >
+//             <AddIcon />
+//             Add new list
+//           </Box>
+//         </Stack>
+//       </Box>
+//     </Container>
+//   )
+// }
